7f78edeb15b34a1d8053352a60dbed2ceeafa248
agentsimon <31639346+agentsimon@users.noreply.github.com>
2021-01-26 15:56:26 +0700

    Update joke.py

--- joke.py
+++ joke.py
@@ -1,9 +1,10 @@
 
+
 import sys
 import os
 print(str(sys.path))
 import pyttsx3
-import pandas as pd
+import csv
 import RPi.GPIO as GPIO
 import time
 
@@ -13,34 +14,39 @@ from random import seed
 from random import randint
 # seed random number generator
 seed(1)
+GPIO_PIN = 24
 GPIO.setmode(GPIO.BCM)
+csvFileArray = []
 
-sensor = 17
+GPIO.setup(GPIO_PIN, GPIO.IN)
 
-#GPIO.setup(sensor, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 file_name = '/home/pi/Documents/Joke project/jokes.csv'
-jokes_list = pd.read_csv(file_name, sep='delimiter', header=None)
-#find number of rows to use as random number generator 
-random_tot = (jokes_list.shape[0])-1
+jokes_list = open(file_name, 'r')
+
+for row in csv.reader(jokes_list, delimiter = '.'):
+  csvFileArray.append(row)
+total_row = len(csvFileArray)
 #Function executed on signal detection
-# def active(null):
-        # #value = randint(0, random_tot)
-        # joke = jokes_list.loc[randint(0, random_tot)]
-        # engine = pyttsx3.init()
-        # engine.say(joke)
-        # engine.runAndWait()
+def active(null):
+        #value = randint(0, random_tot)
+        joke = csvFileArray[randint(0, total_row)]
+        print(joke)
+        converter = pyttsx3.init()
+        # Sets speed percent  
+# Can be more than 100 
+        converter.setProperty('rate', 120) 
+# Set volume 0-1 
+        converter.setProperty('volume', 0.7) 
+        converter.say(joke)
+        converter.runAndWait()
 
 #On detecting signal (falling edge), active function will be activated.
-#GPIO.add_event_detect(sensor, GPIO.FALLING, callback=active, bouncetime=100) 
+GPIO.add_event_detect(GPIO_PIN, GPIO.FALLING, callback=active, bouncetime=100) 
   
 # main program loop
 try:
         while True:
-                joke = jokes_list.loc[randint(0, random_tot)]
-                engine = pyttsx3.init()
-                engine.say(joke)
-                print(joke)
-                engine.runAndWait()
+                
                 time.sleep(1)
   
 # Scavenging work after the end of the program
