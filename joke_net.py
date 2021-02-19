d5e330f47c97591b67c24f86958ef54efab1ab90
agentsimon <31639346+agentsimon@users.noreply.github.com>
2021-02-19 10:36:18 +0700

    Update joke_net.py

--- joke_net.py
+++ joke_net.py
@@ -1,27 +1,55 @@
-import requests
-import json
+import pandas as pd
+import RPi.GPIO as GPIO
+import time
 
+GPIO.setmode(GPIO.BCM)
 
-def jokes(f):
-    
-    data = requests.get(f)
-    tt = json.loads(data.text)
-    return tt
+sensor = 24
 
-f = r"https://official-joke-api.appspot.com/random_joke"
-a = jokes(f)
+GPIO.setup(sensor, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+file_name = "jokes.csv"
+jokes_list = pd.read_csv(file_name)
+  
+#Function executed on signal detection
+def active(null):
+       try:
+		   request = requests.get(url, timeout=timeout)==1
+			print("Connected to the Internet")
+			data = requests.get(f)
+			a = json.loads(data.text)
+			part1 = (a["setup"],)
+			part2 = (a["punchline"],)
+			joke = part1 + part2
+			print(joke)
+		
 
-
-input = GPIO.input(24)
-    if ((not prev_input) and input):
-		joke_getter
-		converter = pyttsx3.init()
+		except (requests.ConnectionError, requests.Timeout) as exception:
+			print("No internet connection.")
+			file_name = '/home/pi/Documents/Jokeproject/jokes.csv'
+			jokes_list = open(file_name, 'r')
+			csvFileArray = []
+			for row in csv.reader(jokes_list, delimiter = '.'):
+				csvFileArray.append(row)
+			total_row = len(csvFileArray)
+			joke = csvFileArray[randint(0, total_row)]
+	
+        converter = pyttsx3.init()
         # Sets speed percent  
     # Can be more than 100 
 	# converter.setProperty('rate', 120) 
     # Set volume 0-1 
-		converter.setProperty('volume', 1) 
-		converter.say(joke)
-		converter.runAndWait()
-		prev_input =input
-    	time.sleep(0.05)
+	    converter.setProperty('volume', 1) 
+	    converter.say(joke)
+	    converter.runAndWait()
+
+#On detecting signal (falling edge), active function will be activated.
+GPIO.add_event_detect(sensor, GPIO.FALLING, callback=active, bouncetime=100) 
+  
+# main program loop
+try:
+        while True:
+                time.sleep(0.05)
+  
+# Scavenging work after the end of the program
+except KeyboardInterrupt:
+        GPIO.cleanup()
