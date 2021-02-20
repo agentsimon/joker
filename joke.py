1c05c7d49de33e5b03ece6e8a75fcbc61e373e03
agentsimon <31639346+agentsimon@users.noreply.github.com>
2021-02-20 14:19:39 +0700

    Update joke.py

--- joke.py
+++ joke.py
@@ -1,50 +1,75 @@
-#! /usr/bin/python2.7
-import sys
-import os
-print(str(sys.path))
 import pyttsx3
-import csv
+import json
+import requests
+from urllib.request import urlopen
+import pandas as pd
+import numpy as np
 import RPi.GPIO as GPIO
+
 import time
 
-
-# setup everything
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(24,GPIO.IN)
-count = 0
-prev_input = 0
-csvFileArray = []
-# generate random integer values
-from random import seed
-from random import randint
-# seed random number generator
-seed(1)
 
-#Read the CSV file
-file_name = '/home/pi/Documents/joker-main/jokes.csv'
-jokes_list = open(file_name, 'r')
-csvFileArray = []
-for row in csv.reader(jokes_list, delimiter = '.'):
-  csvFileArray.append(row)
-total_row = len(csvFileArray)
+sensor = 24
 
+GPIO.setup(sensor, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+file_name = "jokes.csv"
+#jokes_list = pd.read_csv("/home/pi/Documents/joker-main/" +file_name)
+joke = []
+f = r"https://official-joke-api.appspot.com/random_joke"
+#Function executed on signal detection
+def active(null):
+	try:
+			url = "https://www.google.com"
+			file=urlopen(url)
+			status = "Connected"
+			
+	except:
+			print("No internet connection.")
+			file_name = '/home/pi/Documents/joker-main/jokes.csv'
+			jokes_list = open(file_name, 'r')
+			csvFileArray = []
+			for row in csv.reader(jokes_list, delimiter = '.'):
+				csvFileArray.append(row)
+			total_row = len(csvFileArray)
+			joke = csvFileArray[randint(0, total_row)]
+	if status == "Connected":
+		print("Connected to the Internet")
+		
+		data = requests.get(f)
+		a = json.loads(data.text)
+		part1 = (a["setup"],)
+		part2 = (a["punchline"],)
+		joke = part1 + part2
+    
+	else:
+		print("No internet connection.")
+		file_name = '/home/pi/Documents/Jokeproject/jokes.csv'
+		jokes_list = open(file_name, 'r')
+		csvFileArray = []
+		for row in csv.reader(jokes_list, delimiter = '.'):
+			csvFileArray.append(row)
+		total_row = len(csvFileArray)
+		joke = csvFileArray[randint(0, total_row)]
 
-try:
-    while True:
-        input = GPIO.input(24)
-        if ((not prev_input) and input):
-            joke = csvFileArray[randint(0, total_row)]
-            print(joke)
-            converter = pyttsx3.init()
-            # Sets speed percent  
+	converter = pyttsx3.init()
+        # Sets speed percent  
     # Can be more than 100 
-            converter.setProperty('rate', 120) 
+	converter.setProperty('rate', 150) 
     # Set volume 0-1 
-            converter.setProperty('volume', 1) 
-            converter.say(joke)
-            converter.runAndWait()
-        prev_input =input
-        time.sleep(0.05)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+	converter.setProperty('volume', 1) 
+	converter.say(joke)
+	converter.runAndWait()
 
+#On detecting signal (falling edge), active function will be activated.
+GPIO.add_event_detect(sensor, GPIO.FALLING, callback=active, bouncetime=100) 
+
+
+# main program loop
+try:
+        while True:
+                time.sleep(0.05)
+  
+# Scavenging work after the end of the program
+except KeyboardInterrupt:
+        GPIO.cleanup()
